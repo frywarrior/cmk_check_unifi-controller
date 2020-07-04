1ccb2e9b11f6c646bf609ebf61f6b1fb813043f9
BinaryBear <maxspangenberg@web.de>
2020-07-04 23:50:33 +0200

    Update check_unifi-controller.sh

--- check_unifi-controller.sh
+++ check_unifi-controller.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # script to list all UniFi devices from the given controller and get some infos
 # https://github.com/binarybear-de/cmk_check_unifi-controller
-# version 2020-06-09
+# version 2020-07-04
 
 ###############################################################
 
 # UniFi Settings
 USERNAME=someuser
 PASSWORD=somepass
-SITE=office
-BASEURL=https://demo.ui.com:443
+BASEURL=https://demo.ui.com
+CURLOPTS="" #the --insecure option is needed if your controller has a self-signed certificate
 
 # script settings
 STATUS_PROVISIONING=1 #state when provisioning
@@ -29,71 +29,74 @@ getValue() {
 }
 
 DEVICES_FILE=/tmp/unifi-check-devices$$ #store the complete list of all devices found
+SITES_FILE=/tmp/unifi-check-sites$$ #store the complete list of all sites found
 COOKIE_FILE=/tmp/unifi-check-cookie$$ #cookie for the logged in session
-CURL_CMD="curl --tlsv1.2 --silent --cookie ${COOKIE_FILE} --cookie-jar ${COOKIE_FILE} --insecure " #the curl command used to pull the data from the WebUI
+CURL_CMD="curl --tlsv1.2 --silent --cookie ${COOKIE_FILE} --cookie-jar ${COOKIE_FILE} $CURLOPTS" #the curl command used to pull the data from the WebUI
 
-# create the temporary devices and cookies file
-rm -f $DEVICES_FILE $COOKIE_FILE
-touch $DEVICES_FILE $COOKIE_FILE
-chmod 600 $DEVICES_FILE $COOKIE_FILE
+# create the temporary files
+rm -f $DEVICES_FILE $COOKIE_FILE $SITES_FILE
+touch $DEVICES_FILE $COOKIE_FILE $SITES_FILE
+chmod 600 $DEVICES_FILE $COOKIE_FILE $SITES_FILE
 
-${CURL_CMD} --data "{\"username\":\"$USERNAME\", \"password\":\"$PASSWORD\"}" $BASEURL/api/login > /dev/null #log into the controller
-${CURL_CMD} --data "{}" $BASEURL/api/s/$SITE/stat/device > $DEVICES_FILE #get all devices on that site
-${CURL_CMD} $BASEURL/logout #finally close the session
+# login and get site list
+${CURL_CMD} --data "{\"username\":\"$USERNAME\", \"password\":\"$PASSWORD\"}" $BASEURL/api/login > /dev/null
+${CURL_CMD} $BASEURL/api/self/sites > $SITES_FILE
+SITES_NAME=$(cat $SITES_FILE | jq '.data | .[] | .name') #gets all site names
 
-SERIALS=$(cat $DEVICES_FILE | jq '.data | .[] | .serial') # iterate over the acquired serials
+# loop over all sites
+for SITE in $(echo $SITES_NAME | sed -e 's/"//g'); do
 
-# check the attributes below for each device
-for S in $SERIALS; do
-	DEVICE_NAME=$(getValue $S .name | sed -e 's/"//g')
-	CLIENTS=$(getValue $S .num_sta )
-	LASTSEEN=$(getValue $S .last_seen | sed -e 's/"//g')
-	UPGRADEABLEFW=$(getValue $S .upgrade_to_firmware | sed -e 's/"//g')
-	UPGRADEABLE=$(getValue $S .upgradable | sed -e 's/"//g')
-	UPDATING=$(getValue $S .upgrade_state | sed -e 's/"//g')
-	VERSION=$(getValue $S .version | sed -e 's/"//g')
-	LOAD1=$(getValue $S .sys_stats.loadavg_1 | sed -e 's/"//g' )
-	LOAD5=$(getValue $S .sys_stats.loadavg_5 | sed -e 's/"//g' )
-	LOAD15=$(getValue $S .sys_stats.loadavg_15 | sed -e 's/"//g' )
-	STATE=$(getValue $S .state | sed -e 's/"//g')
-	UPLINK_DP=$(getValue $S .uplink.full_duplex)
-	UPLINK_SP=$(getValue $S .uplink.max_speed)
+	${CURL_CMD} --data "{}" $BASEURL/api/s/$SITE/stat/device > $DEVICES_FILE #get all devices on that site
+	SERIALS=$(cat $DEVICES_FILE | jq '.data | .[] | .serial') # iterate over the acquired serials
 
-	STATUS=3 # set the service-state in check_mk, default is unknown if something weird happens
+	# loop over all serial numbers (devices) on current site
+	for SERIAL in $SERIALS; do
+		DEVICE_NAME=$(getValue $SERIAL .name | sed -e 's/"//g; s/ //g')
+		CLIENTS=$(getValue $SERIAL .num_sta )
+		LASTSEEN=$(getValue $SERIAL .last_seen | sed -e 's/"//g')
+		UPGRADEABLEFW=$(getValue $SERIAL .upgrade_to_firmware | sed -e 's/"//g')
+		UPGRADEABLE=$(getValue $SERIAL .upgradable | sed -e 's/"//g')
+		UPDATING=$(getValue $SERIAL .upgrade_state | sed -e 's/"//g')
+		VERSION=$(getValue $SERIAL .version | sed -e 's/"//g')
+		STATE=$(getValue $SERIAL .state | sed -e 's/"//g')
+		UPLINK_DP=$(getValue $SERIAL .uplink.full_duplex)
+		UPLINK_SP=$(getValue $SERIAL .uplink.max_speed)
 
-	# determinate the device's state
-	if [ $STATE -eq 1 ]; then
-		STATUS=0
-		DESCRIPTION="CONNECTED"
-	elif [ $STATE -eq 5 ]; then
-		STATUS=$STATUS_PROVISIONING
-		DESCRIPTION="PROVISIONING"
-	elif [ $STATE -eq 4 ]; then
-		STATUS=$STATUS_UPGRADING
-		DESCRIPTION="UPGRADING"
-	elif [ $STATE -eq 6 ]; then
-		STATUS=1
-		DESCRIPTION="heartbeat missed!"
-	elif [ $STATE -eq 0 ]; then
-		STATUS=2
-		DESCRIPTION="DISCONNECTED!"
-	else
-		STATUS=3
-		DESCRIPTION="Unkown state $STATE!"
-	fi
+		STATUS=3 # set the service-state in check_mk, default is unknown if something weird happens
 
-	# make a upgrade check
-	if [ $UPGRADEABLE = "true" ]; then
-		UPDATESTRING=" ($UPGRADEABLEFW avaible)"
-		if [ $STATUS -eq 0 ]; then STATUS=$STATUS_UPGRADABLE; fi
-	else
-		UPDATESTRING=""
-	fi
+		# determinate the device's state
+		if [ $STATE -eq 1 ]; then
+			STATUS=0
+			DESCRIPTION="CONNECTED"
+		elif [ $STATE -eq 5 ]; then
+			STATUS=$STATUS_PROVISIONING
+			DESCRIPTION="PROVISIONING"
+		elif [ $STATE -eq 4 ]; then
+			STATUS=$STATUS_UPGRADING
+			DESCRIPTION="UPGRADING"
+		elif [ $STATE -eq 6 ]; then
+			STATUS=1
+			DESCRIPTION="heartbeat missed!"
+		elif [ $STATE -eq 0 ]; then
+			STATUS=2
+			DESCRIPTION="DISCONNECTED!"
+		else
+			STATUS=3
+			DESCRIPTION="Unkown state $STATE!"
+		fi
 
-	echo "$STATUS UniFi_$DEVICE_NAME clients=$CLIENTS|load1=$LOAD1|load5=$LOAD5|load15=$LOAD15 $DESCRIPTION, Site: $SITE, last connection: $(date -d @$LASTSEEN '+%F %T'), Clients: $CLIENTS, Firmware: $VERSION$UPDATESTRING" #check_mk output
+		# make a upgrade check
+		if [ $UPGRADEABLE = "true" ]; then
+			UPDATESTRING=" ($UPGRADEABLEFW avaible)"
+			if [ $STATUS -eq 0 ]; then STATUS=$STATUS_UPGRADABLE; fi
+		else
+			UPDATESTRING=""
+		fi
 
+		echo "$STATUS UniFi_$DEVICE_NAME clients=$CLIENTS $DESCRIPTION, Site: $SITE, last connection: $(date -d @$LASTSEEN '+%F %T'), Clients: $CLIENTS, Firmware: $VERSION$UPDATESTRING" #check_mk output
+	done
 done
 echo "0 UniFi_Controller - Version $(dpkg -l unifi | grep ii | awk {'print $3'})" #output the controllers version
 
-# clean temporary files from earlier
-rm -f $DEVICES_FILE $COOKIE_FILE
+${CURL_CMD} $BASEURL/logout #finally close the session
+rm -f $DEVICES_FILE $COOKIE_FILE $SITES_FILE # clean temporary files from earlier
