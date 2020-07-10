c9e544c3bb7155449e22ab2146a50b592c6d935c
BinaryBear <maxspangenberg@web.de>
2020-07-10 15:05:11 +0200

    bug-fix, multi-site autodetection
    - reworked the state-mapping to a switch-case
    - added controller version-check via API
    - added multi-site autodetection
    - reduced parsed metrics (killed LOAD1 and LOAD15
    - reworked update-check of AP to compare current and avaiable version

--- check_unifi-controller.sh
+++ check_unifi-controller.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
-# script to list all UniFi devices from the given controller and get some infos
-# https://github.com/binarybear-de/cmk_check_unifi-controller
-# version 2020-07-04
+# script to list all UniFi devices from the given controller and get some infos https://github.com/binarybear-de/cmk_check_unifi-controller
+# version 2020-07-10
 
 ###############################################################
 
-# UniFi Settings
+# Settings for the controller-binding
 USERNAME=someuser
 PASSWORD=somepass
 BASEURL=https://demo.ui.com
-CURLOPTS="" #the --insecure option is needed if your controller has a self-signed certificate
 
-# script settings
+# Only set site if you DONT want them to be autodetected!
+SITES=office
+
+# additional curl options
+# - insecure-flag is needed if a self-signed certificate is used and is not imported in linux - usually okay if controller is running locally
+# - enforce a specific version of TLS
+CURLOPTS=" --insecure --tlsv1.2"
+
+# mapping of device's states to check_mk statuses
+# STATUS: 0 = OK, 1 = WARN, 2 = CRIT, 3 = UNKN
 STATUS_PROVISIONING=1 #state when provisioning
 STATUS_UPGRADING=1 # state when firmware upgrade is in progress
 STATUS_UPGRADABLE=0 #state when firmware updates are available
-# STATUS: 0 = OK, 1 = WARN, 2 = CRIT, 3 = UNKN
+STATUS_HEARTBEAT_MISSED=1 #state when controller missed some heartbeat-'pings'
 
 ###############################################################
 
@@ -28,75 +35,92 @@ getValue() {
 	echo $VALUE
 }
 
-DEVICES_FILE=/tmp/unifi-check-devices$$ #store the complete list of all devices found
-SITES_FILE=/tmp/unifi-check-sites$$ #store the complete list of all sites found
-COOKIE_FILE=/tmp/unifi-check-cookie$$ #cookie for the logged in session
-CURL_CMD="curl --tlsv1.2 --silent --cookie ${COOKIE_FILE} --cookie-jar ${COOKIE_FILE} $CURLOPTS" #the curl command used to pull the data from the WebUI
+# define temporary files
+FILE_SUFFIX=$$ #random seed (mainly to keep the suffix the same for debugging purposes)
+FILE_PREFIX=/tmp/unifi-check
+
+DEVICES_FILE=$FILE_PREFIX-devices-$RND_SEED #file for a list of all devices
+SITES_FILE=$FILE_PREFIX-sites-$RND_SEED #file with list of all sites
+COOKIE_FILE=$FILE_PREFIX-cookie-$RND_SEED #cookie for the logged in session
+STATUS_FILE=$FILE_PREFIX-status-$RND_SEED #file with controller status
+CURL_CMD="curl --silent --cookie ${COOKIE_FILE} --cookie-jar ${COOKIE_FILE} $CURLOPTS" #the curl command used to pull the data from the WebUI
 
 # create the temporary files
-rm -f $DEVICES_FILE $COOKIE_FILE $SITES_FILE
-touch $DEVICES_FILE $COOKIE_FILE $SITES_FILE
-chmod 600 $DEVICES_FILE $COOKIE_FILE $SITES_FILE
+rm -f $DEVICES_FILE $COOKIE_FILE $SITES_FILE $STATUS_FILE #remove them if they already exist
+touch $DEVICES_FILE $COOKIE_FILE $SITES_FILE $STATUS_FILE #create a empty file
+chmod 600 $DEVICES_FILE $COOKIE_FILE $SITES_FILE $STATUS_FILE #set permissions strictly
 
-# login and get site list
-${CURL_CMD} --data "{\"username\":\"$USERNAME\", \"password\":\"$PASSWORD\"}" $BASEURL/api/login > /dev/null
-${CURL_CMD} $BASEURL/api/self/sites > $SITES_FILE
-SITES_NAME=$(cat $SITES_FILE | jq '.data | .[] | .name') #gets all site names
+###############################################################
+
+${CURL_CMD} --data "{\"username\":\"$USERNAME\", \"password\":\"$PASSWORD\"}" $BASEURL/api/login > /dev/null #login to the controller
+
+# get some basic information about the controller
+${CURL_CMD} $BASEURL/api/s/default/stat/sysinfo > $STATUS_FILE
+CONTROLLERBUILD=$(cat $STATUS_FILE | jq '.data | .[] | .build' | sed -e 's/"//g')
+STATUS=0
+if [ $(cat $STATUS_FILE | jq '.data | .[] | .update_available') = "true" ]; then
+	CONTROLLERBUILD="$CONTROLLERBUILD (Update avaiable!)"
+	STATUS=$STATUS_UPGRADABLE
+fi
+
+echo "$STATUS UniFi-Controller - Build $CONTROLLERBUILD" #output the controllers version
+
+# check if site auto-detection should be used when no sites were specified
+if ! [ $SITES ]; then
+	${CURL_CMD} $BASEURL/api/self/sites > $SITES_FILE #write found sites to file
+	SITES_NAME=$(cat $SITES_FILE | jq '.data | .[] | .name') #gets all site names
+	SITES=$(echo $SITES_NAME | sed -e 's/"//g')
+fi
 
 # loop over all sites
-for SITE in $(echo $SITES_NAME | sed -e 's/"//g'); do
+for SITE in $SITES; do
 
 	${CURL_CMD} --data "{}" $BASEURL/api/s/$SITE/stat/device > $DEVICES_FILE #get all devices on that site
 	SERIALS=$(cat $DEVICES_FILE | jq '.data | .[] | .serial') # iterate over the acquired serials
 
 	# loop over all serial numbers (devices) on current site
-	for SERIAL in $SERIALS; do
-		DEVICE_NAME=$(getValue $SERIAL .name | sed -e 's/"//g; s/ //g')
-		CLIENTS=$(getValue $SERIAL .num_sta )
-		LASTSEEN=$(getValue $SERIAL .last_seen | sed -e 's/"//g')
-		UPGRADEABLEFW=$(getValue $SERIAL .upgrade_to_firmware | sed -e 's/"//g')
-		UPGRADEABLE=$(getValue $SERIAL .upgradable | sed -e 's/"//g')
-		UPDATING=$(getValue $SERIAL .upgrade_state | sed -e 's/"//g')
-		VERSION=$(getValue $SERIAL .version | sed -e 's/"//g')
-		STATE=$(getValue $SERIAL .state | sed -e 's/"//g')
-		UPLINK_DP=$(getValue $SERIAL .uplink.full_duplex)
-		UPLINK_SP=$(getValue $SERIAL .uplink.max_speed)
+	for S in $SERIALS; do
+		DEVICE_NAME=$(getValue $S .name | sed -e 's/"//g; s/\ /_/g')
+		CLIENTS=$(getValue $S .num_sta)
+		LOAD=$(getValue $S .sys_stats.loadavg_5 | sed -e 's/"//g; s/\ /_/g')
+		UPGRADEABLEFW=$(getValue $S .upgrade_to_firmware | sed -e 's/"//g')
+		VERSION=$(getValue $S .version | sed -e 's/"//g')
+		STATE=$(getValue $S .state)
 
 		STATUS=3 # set the service-state in check_mk, default is unknown if something weird happens
 
-		# determinate the device's state
-		if [ $STATE -eq 1 ]; then
-			STATUS=0
-			DESCRIPTION="CONNECTED"
-		elif [ $STATE -eq 5 ]; then
-			STATUS=$STATUS_PROVISIONING
-			DESCRIPTION="PROVISIONING"
-		elif [ $STATE -eq 4 ]; then
-			STATUS=$STATUS_UPGRADING
-			DESCRIPTION="UPGRADING"
-		elif [ $STATE -eq 6 ]; then
-			STATUS=1
-			DESCRIPTION="heartbeat missed!"
-		elif [ $STATE -eq 0 ]; then
-			STATUS=2
-			DESCRIPTION="DISCONNECTED!"
-		else
-			STATUS=3
-			DESCRIPTION="Unkown state $STATE!"
-		fi
+		# determining the device's state
+		# https://community.ui.com/questions/Fetching-current-UAP-status/88a197f9-3530-4580-8f0b-eca43b41ba6b
+		case $STATE in
+			1)      STATUS=0
+				DESCRIPTION="CONNECTED";;
+
+			0)      STATUS=2
+				DESCRIPTION="DISCONNECTED!";;
+
+			4)      STATUS=$STATUS_UPGRADING
+				DESCRIPTION="UPGRADING";;
+
+			5)      STATUS=$STATUS_PROVISIONING
+				DESCRIPTION="PROVISIONING";;
+
+			6)      STATUS=$STATUS_HEARTBEAT_MISSED
+				DESCRIPTION="heartbeat missed!";;
+
+			*)      DESCRIPTION="Unkown state ($STATE)!";;
+			esac
 
 		# make a upgrade check
-		if [ $UPGRADEABLE = "true" ]; then
+		if [ $UPGRADEABLEFW != $VERSION ]; then
 			UPDATESTRING=" ($UPGRADEABLEFW avaible)"
 			if [ $STATUS -eq 0 ]; then STATUS=$STATUS_UPGRADABLE; fi
 		else
 			UPDATESTRING=""
 		fi
 
-		echo "$STATUS UniFi_$DEVICE_NAME clients=$CLIENTS $DESCRIPTION, Site: $SITE, last connection: $(date -d @$LASTSEEN '+%F %T'), Clients: $CLIENTS, Firmware: $VERSION$UPDATESTRING" #check_mk output
+		echo "$STATUS UniFi_$DEVICE_NAME clients=$CLIENTS|load=$LOAD $DESCRIPTION, Site: $SITE, Clients: $CLIENTS, Firmware: $VERSION$UPDATESTRING" #check_mk output
 	done
 done
-echo "0 UniFi_Controller - Version $(dpkg -l unifi | grep ii | awk {'print $3'})" #output the controllers version
 
 ${CURL_CMD} $BASEURL/logout #finally close the session
-rm -f $DEVICES_FILE $COOKIE_FILE $SITES_FILE # clean temporary files from earlier
+rm -f $DEVICES_FILE $COOKIE_FILE $SITES_FILE $STATUS_FILE # clean temporary files from earlier
